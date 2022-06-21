747d173a30e3c5640b8b912aded314df38f5903c
yakupdygn <y.dygn66@hotmail.com>
2022-06-21 12:00:39 +0200

    sa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+import SwiperCore, { EffectCards } from "swiper";
 
+SwiperCore.use([EffectCards]);
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'test';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
+import { TestComponent } from './test/test.component';
+import { FormsModule } from "@angular/forms";
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule,
+    
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
